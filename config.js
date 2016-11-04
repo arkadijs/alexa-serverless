b6a702e8b820fa0de3a1d4872d12f769e91829c1
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2016-11-04 09:23:46 +0200

    Timetable update

--- config.js
+++ config.js
@@ -8,29 +8,30 @@ exports.timetable = {
             '12:00-12:45': 'Workshop opening, plans and agenda / Madrid / Nigel Barnes and Ezio Armando',
             '12:55-13:50': [
                 'Whats New in NewIT / Madrid / Max Furmanov / innovation',
-                'Continuous Monitoring with Splunk / Malta / Stanley Kaplunov / devops',
+                'Continuous Monitoring with Splunk / Malta / Stanley Kaplunov and Juergen Magiera / devops',
                 'Introduction to Lightweight Architecture / Oslo / Sven Loberg and Matt Lancaster / microservices',
                 'Evolution of ITIL Point of View / Helsinki / Martin Croker / arch',
-                'AWS Security with HoneyBee & Accenture DevOps Platform / Monaco / Ian Finch and Andi Strain / cloud'
+                'AWS Security with HoneyBee and Accenture DevOps Platform / Amsterdam / Ian Finch and Andi Strain / cloud'
             ],
             '13:50-14:50': 'Lunch / Praga',
             '15:00-15:55': [
-                'State Of DevOps 2016 / Madrid / Mark Solomon / devops',
+                'State Of DevOps 2016 / Madrid / Andrew Howard / devops',
                 'The journey of architecting and delivering a platform ecosystem based on Azure / Malta / Tobias Maier and Stefan Ruehl / cloud',
-                'Lean Engineering Applied / Oslo / Regis Allegre / innovation',
+                'Lean Engineering Applied / Oslo / Regis Allegre and Martin Lofaj / innovation',
                 'Liquid Platform: the new way to crowdsource your project / Helsinki / Arnaud Dolignon and Kevin Bader / innovation'
             ],
-            '15:00-17:00': 'Microservices hands on with Node js and Docker in the Cloud / Monaco / Ilkka Anttonen / microservices',
+            '15:00-17:00': 'Hands on Lightweight Modern Microservices Workshop / Amsterdam / Ilkka Anttonen and Hugo Alves / microservices',
             '16:05-17:00': [
-                'Rotating EON to the new. A major European utilities company transforming itself / Madrid / Thomas Michelbach and Julio Sincero / innovation',
+                'Open Source: State of the Nation within Accenture / Madrid / Sven Loberg / arch',
                 'Dev Sec Ops / Malta / R. Blanco Rincón / devops',
-                'Open Source: State of the Nation within Accenture / Oslo / Sven Loberg / arch',
+                'Rotating EON to the new. A major European utilities company transforming itself / Oslo / Thomas Michelbach and Julio Sincero / innovation',
                 'No servers, No worries – An Introduction to NoOps and Serverless Computing / Helsinki / Roman Schiefer and Arkadi Shishlov / cloud'
             ],
             '17:15-18:10': [
-                'The FS Reference Application / Malta / Slavomir Kubacka / microservices',
-                'Introduction to the new A4 Tool / Oslo / Olivier Fortin & Ana Osman / innovation',
-                'Continuous Monitoring with Splunk / Helsinki / Stanley Kaplunov / devops'
+                'Case Study: Using New IT to build a modern Integration Architecture for an Investment Bank / Malta / Andris Nestors / arch',
+                'Introduction to the new A4 Tool / Oslo / Olivier Fortin and Ana Osman / innovation',
+                'How We Created a High Performing DevOps team at Argos / Helsinki / Pulak Agrawal / devops',
+                'The FS Reference Application / Amsterdam / Slavomir Kubacka / microservices',
             ],
             '18:20-19:20': 'Technology Strategy / Virtual / Nicola Morini Bianzino',
             '19:30-23:00': 'Dinner and Cocktails / Florencia'
@@ -38,61 +39,61 @@ exports.timetable = {
         },
         '11-08': {
             '07:00-08:50': 'Breakfast / Praga',
-            '09:00-09:55': 'Tech Vision and Tech Labs / Madrid / Emmanuel Viale',
-            '09:55-10:40': 'Performance / Madrid / David Golding',
+            '09:00-09:55': 'Tech Vision and Tech Labs Virtual Visit / Madrid / Emmanuel Viale',
+            '09:55-10:40': 'A Word from David / Madrid / David Golding',
             '11:00-11:55': [
-                'Accenture Cloud First Applications overview: Deep dive into Salesforce.com / Madrid / Ivan Almeida / cloud',
-                'Elhub – The centralized datahub for the Norwegian Power Market. Architecture and Integration Patterns / Malta / Knut-Erik Johnsen andJon André Fagerholm / arch',
-                'Microservices Reference Architecture / Copenhague / Anselm Ringleben & Christian Muuss / microservices',
-                'The rise of Virtual Agent and Chat Bots / Helsinki / Laetitia Cailleteau / innovation',
-
+                'The rise of Virtual Agent and Chat Bots / Madrid / Laetitia Cailleteau and Simon De Timary / innovation',
+                'Microservices Reference Architecture / Malta / Anselm Ringleben and Christian Muuß / microservices',
+                'Elhub – The centralized datahub for the Norwegian Power Market. Architecture and Integration Patterns / Copenhague / Knut-Erik Johnsen and Jon André Fagerholm / arch',
+                'Accenture Cloud First Applications overview: Deep dive into Salesforce.com / Helsinki / Ivan Almeida / cloud'
             ],
             '11:00-13:05': 'Design Thinking - Hands on / Monaco / Andrea Smyth / innovation',
             '12:10-13:05': [
                 'Creating sharable real NewIT that just works / Madrid / Mark Rendell / devops',
-                'Circus / Malta / Google / cloud',
-                'New Microservices arch for a Large Retailer / Copenhague / Jose Luis Martin / microservices',
-                'Case study: High volume, end-to- end eCommerce implementation / Helsinki / Joakim Jonsson / arch'
+                'Modern Data Platforms in the Age of Microservices / Malta / Dominik Wagenknecht / microservices',
+                'Artificial Intelligence Iberia: Capabilities and Demos / Copenhague / Angel Morales and Roberto Diñeiro / innovation',
+                'Mind the gap: An Internet of Things journey / Helsinki / Ana Jerez Martín / cloud'
+
             ],
             '13:15-14:40': 'Lunch / Praga',
             '14:45-15:40': [
-                'Mind the gap: An Internet of Things journey / Madrid / Ana Jerez Martín / cloud',
-                'Accenture Insights Platform / Malta / Will Gatehouse / cloud',
-                'State of the art of Mainframe Downsizing / Copenhague / Roberto Ucero / arch',
-                'Modern Data Platforms in the Age of Microservices / Helsinki / Dominik Wagenknecht / microservices'
+                'State of the art of Mainframe Downsizing / Madrid / Roberto Ucero / arch',
+                'Accenture Container Reference Architecture / Malta / Thomas Rosqvist / microservices',
+                'Digital Native Architectures Eat the World: Microservices and Reactive Invasion / Copenhague / Matt Lancaster / innovation',
+                'Accenture Insights Platform / Helsinki / Will Gatehouse / cloud',
             ],
             '14:45-16:45': 'Design Thinking - Hands on / Monaco / Andrea Smyth',
             '15:50-16:45': [
-                'Blockchain and UKI Innovation Program / Madrid / Sarah Francis, Laurence Freeman and Callum Hyland / innovation',
-                'Modern Infrastructure Architecture – the journey that teaches you the destination / Malta / Andi Strain / devops',
-                'Serverless on Accenture Cloud Platform / Copenhague / Roger Foskett / cloud',
-                'Accenture Container Reference Architecture / Helsinki / Thomas Rosqvist / microservices'
+                'Serverless on Accenture Cloud Platform / Madrid / Roger Foskett / cloud',
+                'Introduction to Lightweight Architecture / Malta / Sven Loberg and Matt Lancaster / microservices',
+                'Blockchain and UKI Innovation Program / Copenhague / Sarah Francis and Laurence Freeman / innovation',
+                'Platform as a Service With Docker and Kubernetes / Helsinki / Steve Fisher / cloud'
             ],
             '17:05-18:00': [
-                'Artificial Intelligence Iberia: Capabilities and Demos / Madrid / Angel Morales and Roberto Diñeiro / innovation',
-                'DevOps for Mainframe / Malta / Marisa Arias Sanz / devops',
-                'Platform as a Service With Docker and Kubernetes / Copenhague / Steve Fisher / cloud',
-                'Introduction to Lightweight Architecture / Helsinki / Sven Loberg and Matt Lancaster / microservices'
+                'No servers, No worries – An Introduction to NoOps and Serverless Computing / Madrid / Roman Schiefer and Arkadi Shishlov / cloud',
+                'Modern Infrastructure Architecture – the journey that teaches you the destination / Malta / Andi Strain / devops',
+                'Case study: High volume, end-to- end eCommerce implementation / Copenhague / Joakim Jonsson / arch',
+                'DevOps for Mainframe / Helsinki / Marisa Arias Sanz / devops'
             ],
-            '17:05-19:05': 'Containers as a Service in the real world + Kubernetes demo / Monaco / Jan Willies / microservices',
+            '17:05-19:05': 'Containers as a Service in the real world + Kubernetes demo / Amsterdam / Jan Willies / microservices',
             '18:10-19:05': [
-                'Serverless on Accenture Cloud Platform / Madrid / Roger Foskett / cloud',
-                'State Of DevOps 2016 / Malta / Mark Solomon / devops',
-                'PSD2 and OpenBanking Architecture – Learnings at UK Banks / Copenhague / Bruno Azenha and Steve Fisher / arch',
-                'Digital Native Architectures Eat the World: Microservices and Reactive Invasion / Helsinki / Matt Lancaster / innovation'
+                'Rotating EON to the new. A major European utilities company transforming itself / Madrid / Thomas Michelbach and Julio Sincero / innovation',
+                'Accenture Container Reference Architecture / Malta / Thomas Rosqvist / microservices',
+                'Serverless on Accenture Cloud Platform / Copenhague / Roger Foskett / cloud',
+                'DevOps and Bimodal IT / Helsinki / Uldis Karlovs Karlovskis / devops'
             ],
             '19:15-23:00': 'Dinner and Cocktails / Florencia'
         },
         '11-09': {
             '07:00-09:00': 'Breakfast / Praga',
             '09:00-09:55': 'Artificial Intelligence / Madrid / Cyrille Bataller',
-            '09:55-10:40': 'Grow Architecture / Madrid / Nigel Barnes and Ezio Armando',
+            '09:55-10:35': 'Grow Architecture / Madrid / Nigel Barnes and Ezio Armando',
             '10:45-11:40': [
-                'Microservices Reference Architecture / Madrid / Anselm Ringleben & Christian Muuss / microservices',
-                'No servers, No worries – An Introduction to NoOps and Serverless Computing / Malta / Roman Schiefer and Arkadi Shishlov / cloud',
-                'Rotating EON to the new. A major European utilities company transforming itself / Copenhague / Thomas Michelbach and Julio Sincero / innovation',
-                'Accenture Container Reference Architecture / Helsinki / Thomas Rosqvist / microservices',
-                'Case Study: Using New IT to build a modern Integration Architecture for an Investment Bank / Monaco / / arch'
+                'New Microservices arch for a Large Retailer / Madrid / Jose Luis Martin / microservices',
+                'Microservices Reference Architecture / Malta / Anselm Ringleben and Christian Muuß / microservices',
+                'PSD2 and OpenBanking Architecture – Learnings at UK Banks / Copenhague / Bruno Azenha and Steve Fisher / arch',
+                'State Of DevOps 2016 / Helsinki / Andrew Howard / devops',
+                'Whats New in NewIT / Amsterdam / Max Furmanov / innovation'
             ],
             '12:00-13:00': 'Update on Accenture\'s Key Architecture Assets, Schools and Certifications / Madrid / Ezio Armando and Olivier Fortin',
             '13:00-13:15': 'Grab box lunch and drink, and find seat in plenary',
